4ef140d3a6109aeb076fe886b8b8e2895ef1dcef
Caine Li <baicai7678@gmail.com>
2017-04-12 10:48:32 -0400

    contants

--- src/PuzzlePiece.java
+++ src/PuzzlePiece.java
@@ -1,6 +1,15 @@
 
 public class PuzzlePiece {
 	
+	public static final int HEART_IN = 1;
+	public static final int HEART_OUT = -1;
+	public static final int DIAMOND_IN = 2;
+	public static final int DIAMOND_OUT = -2;
+	public static final int SPADE_IN = 3;
+	public static final int SPADE_OUT = -3;
+	public static final int CLUB_IN = 4;
+	public static final int CLUB_OUT = -4;
+	
 	private int north;
 	private int south;
 	private int east;
